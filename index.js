9c43e9977f705270c47ab85442912b2e6748bbbc
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-08-04 23:36:09 -0300

    Create Partials and install body parser

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs')
-
 app.use(express.static('public'))
 
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use
+
+// Routes
 app.get('/', (req, res) => {
     res.render("index")
 })
 
 app.get('/perguntar', (req, res) => {
     res.render("perguntar")
+})
+
+app.post('/salvarpergunta', (req, res) => {
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send(`recebido! <br> titulo: ${titulo} <br> descricao: ${descricao}`)
 })
 
 app.listen(7070, () => console.log("Server listening on 7070"))
